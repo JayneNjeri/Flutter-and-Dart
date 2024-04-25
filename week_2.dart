8791c9744a5541b88f957203ccff476edfa69796
JayneNjeri <jaynenjeri739@gmail.com>
2024-04-25 13:27:51 +0300

    Variables

---
+++ week_2.dart
@@ -0,0 +1,23 @@
+void main() {
+  //1. Integers
+  int age = 20;
+  print(age);
+
+  //2. Double
+  double pi = 3.142;
+  print(pi);
+
+  //3. String
+  String name = "Jane Mwangi";
+  print(name);
+
+  //4.List
+  List<String> names = ["Jane", "John", "James"];
+  List<int> ages = [20, 30, 40];
+  print(names);
+  print(ages);
+
+  //5. Maps
+  Map<String, dynamic> person = {"name": "Jane", "age": 20, "isStudent": true};
+  print(person);
+}
